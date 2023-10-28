c43ec907db824fff06e56fdfe31a783334460211
Agustin <76200509+AgusZ02@users.noreply.github.com>
2023-10-28 11:42:15 +0200

    Update EmaitzakIpiniBLBMTest.java

--- src/test/java/EmaitzakIpiniBLBMTest.java
+++ src/test/java/EmaitzakIpiniBLBMTest.java
@@ -2,18 +2,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.spy;
-
 import org.junit.*;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.internal.matchers.Null;
-
 import businessLogic.BLFacade;
 import businessLogic.BLFacadeImplementation;
 import dataAccess.DataAccess;
-import domain.Event;
 import domain.Quote;
 import exceptions.EventNotFinished;
 import org.mockito.ArgumentCaptor;
